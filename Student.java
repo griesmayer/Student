0d9d49ba0b548f730f4d766758b440b5bc4132c5
Thomas Griesmayer <griesmayer@spengergasse.at>
2025-09-09 21:29:56 +0200

    Initial sharing of project

---
+++ Student.java
@@ -0,0 +1,54 @@
+public class Student
+{
+    private String  name;
+    private int     alter;
+    private boolean matura;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public int getAlter()
+    {
+        return alter;
+    }
+    
+    public boolean getMatura()
+    {
+        return matura;
+    }
+    
+    public void setName(String neuName)
+    {
+        name = neuName;
+    }
+    
+    public void setAlter(int neuAlter)
+    {
+        alter = neuAlter;
+    }
+    
+    public void setMatura(boolean neuMatura)
+    {
+        matura = neuMatura;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
